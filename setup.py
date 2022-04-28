049dc65a6937ca5a85542a305c00192b2720705f
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-04-28 19:20:37 +0200

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,15 @@
-"""This module contains the packaging routine for the pybook package"""
-
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
 
-
-def get_requirements(source):
-    """Get the requirements from the given ``source``
-
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
+install_requires = open('./requirements/requirements.txt', 'r').readlines()
 
 setup(
+    name='scrapy-selenium',
+    version='0.0.8',
+    author='Clément Denoix, Péter Ferenc Gyarmati',
+    author_email=', dev.petergy@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/peter-gy/scrapy-selenium',
 )
-
-
